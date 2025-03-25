a56da3827efe4e30b6455bb6ed01129e1df4a948
Daniele Muffato <danixgithub1@gmail.com>
2025-03-25 14:22:19 +0100

    simplification

--- src/payments.rs
+++ src/payments.rs
@@ -3,7 +3,7 @@
 use serde::{Deserialize, Serialize};
 use std::collections::{BTreeSet, HashSet};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 struct ValueSet {
     cities: BTreeSet<String>,
     shops: BTreeSet<String>,
@@ -11,24 +11,14 @@ struct ValueSet {
     items: BTreeSet<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Order {
     unit_price: u32,
     quantity: u32,
     item: String,
 }
 
-impl Order {
-    pub fn new(unit_price: u32, quantity: u32, item: &str) -> Self {
-        Self {
-            unit_price,
-            quantity,
-            item: String::from(item),
-        }
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Payment {
     city: String,
     method: String,
@@ -37,38 +27,8 @@ pub struct Payment {
     orders: BTreeSet<Order>,
 }
 
-impl Payment {
-    pub fn new(city: &str, shop: &str, method: &str, date: i64) -> Self {
-        Self {
-            city: String::from(city),
-            method: String::from(method),
-            shop: String::from(shop),
-            date,
-            orders: BTreeSet::new(),
-        }
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct AllPayments {
     value_set: ValueSet,
     payments: BTreeSet<Payment>,
-}
-
-impl AllPayments {
-    pub fn to_json(&self, pretty: bool) -> Result<String, String> {
-        if pretty {
-            serde_json::to_string_pretty(self).map_err(|err| err.to_string())
-        } else {
-            serde_json::to_string(self).map_err(|err| err.to_string())
-        }
-    }
-
-    pub fn from_json(json: &str) -> Result<Self, String> {
-        serde_json::from_str(json).map_err(|err| err.to_string())
-    }
-
-    pub fn get_payments(&self) -> &BTreeSet<Payment> {
-        &self.payments
-    }
 }
