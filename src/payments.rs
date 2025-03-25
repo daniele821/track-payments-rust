374c4af60a8843b2243cbb53552cb93c8f490abf
Daniele Muffato <danixgithub1@gmail.com>
2025-03-25 14:36:00 +0100

    restructure code: allow different json serializers: one for rust, one for legacy...

--- src/payments.rs
+++ src/payments.rs
@@ -1,34 +1,4 @@
 #![allow(unused)]
 
-use serde::{Deserialize, Serialize};
-use std::collections::{BTreeSet, HashSet};
-
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
-struct ValueSet {
-    cities: BTreeSet<String>,
-    shops: BTreeSet<String>,
-    methods: BTreeSet<String>,
-    items: BTreeSet<String>,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Order {
-    unit_price: u32,
-    quantity: u32,
-    item: String,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Payment {
-    city: String,
-    method: String,
-    shop: String,
-    date: i64,
-    orders: BTreeSet<Order>,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
-pub struct AllPayments {
-    value_set: ValueSet,
-    payments: BTreeSet<Payment>,
-}
+mod json_default;
+mod json_golang;
