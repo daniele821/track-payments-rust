bcb96f148f880465d5b0a9604e1d24d4a7456292
Daniele Muffato <danixgithub1@gmail.com>
2025-03-26 11:06:20 +0100

    json default uses vec instead of btree

--- src/payments/json_default.rs
+++ src/payments/json_default.rs
@@ -12,31 +12,24 @@ pub struct ValueSet {
     items: BTreeSet<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize, Derivative, Clone)]
-#[derivative(PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Order {
     item: String,
     unit_price: u32,
-    #[derivative(PartialEq = "ignore", Ord = "ignore", PartialOrd = "ignore")]
     quantity: u32,
 }
 
-#[derive(Debug, Serialize, Deserialize, Derivative, Clone)]
-#[derivative(PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Payment {
     date: i64,
-    #[derivative(PartialEq = "ignore", Ord = "ignore", PartialOrd = "ignore")]
     city: String,
-    #[derivative(PartialEq = "ignore", Ord = "ignore", PartialOrd = "ignore")]
     method: String,
-    #[derivative(PartialEq = "ignore", Ord = "ignore", PartialOrd = "ignore")]
     shop: String,
-    #[derivative(PartialEq = "ignore", Ord = "ignore", PartialOrd = "ignore")]
-    orders: BTreeSet<Order>,
+    orders: Vec<Order>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AllPayments {
     value_set: ValueSet,
-    payments: BTreeSet<Payment>,
+    payments: Vec<Payment>,
 }
