ddaa4e95a87f5979a51f671412123670f2c35393
Daniele Muffato <danixgithub1@gmail.com>
2025-03-26 13:47:51 +0100

    add derive-getters to avoid boilerplate

--- src/payments/mod.rs
+++ src/payments/mod.rs
@@ -1,12 +1,13 @@
 #![allow(unused)]
 
+use derive_getters::Getters;
 use serde::{Deserialize, Serialize};
 use std::collections::{BTreeMap, BTreeSet};
 
 mod json_default;
 mod json_legacy;
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Getters)]
 pub struct ValueSet {
     cities: BTreeSet<String>,
     shops: BTreeSet<String>,
@@ -14,21 +15,21 @@ pub struct ValueSet {
     items: BTreeSet<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Getters)]
 pub struct OrderId {
     item: String,
     unit_price: u32,
 }
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Getters)]
 pub struct OrderDetail {
     quantity: u32,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Getters)]
 pub struct PaymentId {
     date: i64,
 }
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Getters)]
 pub struct PaymentDetail {
     city: String,
     shop: String,
@@ -36,7 +37,7 @@ pub struct PaymentDetail {
     orders: BTreeMap<OrderId, OrderDetail>,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Getters)]
 pub struct AllPayments {
     value_set: ValueSet,
     payments: BTreeMap<PaymentId, PaymentDetail>,
@@ -59,31 +60,11 @@ impl ValueSet {
             items: BTreeSet::new(),
         }
     }
-
-    pub fn get_cities(&self) -> &BTreeSet<String> {
-        &self.cities
-    }
-    pub fn get_shops(&self) -> &BTreeSet<String> {
-        &self.shops
-    }
-    pub fn get_methods(&self) -> &BTreeSet<String> {
-        &self.methods
-    }
-    pub fn get_items(&self) -> &BTreeSet<String> {
-        &self.items
-    }
 }
 
 impl OrderId {
     pub fn new(item: String, unit_price: u32) -> Self {
         Self { item, unit_price }
-    }
-
-    pub fn get_item(&self) -> &str {
-        &self.item
-    }
-    pub fn get_unitprice(&self) -> u32 {
-        self.unit_price
     }
 }
 
@@ -91,19 +72,11 @@ impl OrderDetail {
     pub fn new(quantity: u32) -> Self {
         Self { quantity }
     }
-
-    pub fn get_quantity(&self) -> u32 {
-        self.quantity
-    }
 }
 
 impl PaymentId {
     pub fn new(date: i64) -> Self {
         Self { date }
-    }
-
-    pub fn get_date(&self) -> i64 {
-        self.date
     }
 }
 
@@ -116,19 +89,6 @@ impl PaymentDetail {
             orders: BTreeMap::new(),
         }
     }
-
-    pub fn get_city(&self) -> &str {
-        &self.city
-    }
-    pub fn get_shop(&self) -> &str {
-        &self.shop
-    }
-    pub fn get_method(&self) -> &str {
-        &self.method
-    }
-    pub fn get_orders(&self) -> &BTreeMap<OrderId, OrderDetail> {
-        &self.orders
-    }
 }
 
 impl AllPayments {
@@ -137,12 +97,5 @@ impl AllPayments {
             value_set: ValueSet::new(),
             payments: BTreeMap::new(),
         }
-    }
-
-    pub fn get_valueset(&self) -> &ValueSet {
-        &self.value_set
-    }
-    pub fn get_payments(&self) -> &BTreeMap<PaymentId, PaymentDetail> {
-        &self.payments
     }
 }
