d08656fb27f80cbefd1573c68057d839930a4b50
Daniele Muffato <danixgithub1@gmail.com>
2025-03-26 13:03:01 +0100

    allpayments common interface

--- src/payments/mod.rs
+++ src/payments/mod.rs
@@ -1,4 +1,43 @@
 #![allow(unused)]
 
+use std::collections::{BTreeMap, BTreeSet};
+use serde::{Deserialize, Serialize};
+
 mod json_default;
 mod json_legacy;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ValueSet {
+    cities: BTreeSet<String>,
+    shops: BTreeSet<String>,
+    methods: BTreeSet<String>,
+    items: BTreeSet<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct OrderId {
+    item: String,
+    unit_price: u32,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct OrderDetail {
+    quantity: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct PaymentId {
+    date: i64,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PaymentDetail {
+    city: String,
+    method: String,
+    shop: String,
+    orders: BTreeMap<OrderId, OrderDetail>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AllPayments {
+    value_set: ValueSet,
+    payments: BTreeMap<PaymentId, PaymentDetail>,
+}
