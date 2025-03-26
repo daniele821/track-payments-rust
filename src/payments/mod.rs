4fd32359d825d915fd4a5cb6bdbfabcbce7edcbf
Daniele Muffato <danixgithub1@gmail.com>
2025-03-26 13:37:05 +0100

    not implement ord traits for structs where it does not make sense

--- src/payments/mod.rs
+++ src/payments/mod.rs
@@ -6,7 +6,7 @@ use std::collections::{BTreeMap, BTreeSet};
 mod json_default;
 mod json_legacy;
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct ValueSet {
     cities: BTreeSet<String>,
     shops: BTreeSet<String>,
@@ -19,7 +19,7 @@ pub struct OrderId {
     item: String,
     unit_price: u32,
 }
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct OrderDetail {
     quantity: u32,
 }
@@ -28,7 +28,7 @@ pub struct OrderDetail {
 pub struct PaymentId {
     date: i64,
 }
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct PaymentDetail {
     city: String,
     shop: String,
@@ -36,13 +36,13 @@ pub struct PaymentDetail {
     orders: BTreeMap<OrderId, OrderDetail>,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct AllPayments {
     value_set: ValueSet,
     payments: BTreeMap<PaymentId, PaymentDetail>,
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum AllPaymentsError {
     OrderDuplicated,
     OrderNotFound,
