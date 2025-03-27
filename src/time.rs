b3e469a281d37bead50b82636eb824870124eb28
Daniele Muffato <danixgithub1@gmail.com>
2025-03-27 03:49:02 +0100

    format_str func

--- src/time.rs
+++ src/time.rs
@@ -1,7 +1,15 @@
 #![allow(unused)]
 
-use chrono::Local;
+use chrono::{DateTime, Local};
+
+pub const CUSTOM_FORMAT: &str = "%Y/%m/%d %H:%M";
 
 pub fn now_fake_utc() -> i64 {
     Local::now().naive_local().and_utc().timestamp()
+}
+
+pub fn format_str(timestamp: i64, format: &str) -> Result<String, String> {
+    DateTime::from_timestamp(timestamp, 0)
+        .map(|date| date.format(format).to_string())
+        .ok_or(String::from("todo"))
 }
