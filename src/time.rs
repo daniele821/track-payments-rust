0e123c3572134e59fe75d5da5bce25a07217617d
Daniele Muffato <danixgithub1@gmail.com>
2025-03-27 04:04:17 +0100

    added string time parser, to fix

--- src/time.rs
+++ src/time.rs
@@ -11,5 +11,27 @@ pub fn now_fake_utc() -> i64 {
 pub fn format_str(timestamp: i64, format: &str) -> Result<String, String> {
     DateTime::from_timestamp(timestamp, 0)
         .map(|date| date.format(format).to_string())
-        .ok_or(String::from("todo"))
+        .ok_or(format!("unable to parse timestamp: {timestamp}"))
+}
+
+pub fn parse_str(time_str: &str, format: &str) -> Result<i64, String> {
+    DateTime::parse_from_str(time_str, format)
+        .map(|timestamp| timestamp.timestamp())
+        .map_err(|err| err.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::time::parse_str;
+
+    use super::{CUSTOM_FORMAT, format_str};
+
+    #[test]
+    pub fn parse_format_time() {
+        let timestamp = 1_743_044_280;
+        let date_str = format_str(timestamp, CUSTOM_FORMAT).unwrap();
+        assert_eq!(date_str, "2025/03/27 02:58");
+        let timestamp2 = parse_str(&date_str, CUSTOM_FORMAT).unwrap();
+        assert_eq!(timestamp2, timestamp);
+    }
 }
