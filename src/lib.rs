2b598a01345832c8fdd9220be4f995a19833ae1b
Daniele Muffato <danixgithub1@gmail.com>
2025-03-29 19:50:12 +0100

    removing comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-//! # `track_payments_rust`
-//!
-//! Crate to manually track payments, and get statistics about them.
-
 pub mod crypto;
 pub mod payments;
 pub mod time;
--- src/time.rs
+++ src/time.rs
@@ -1,33 +1,20 @@
-//! Time related utility functions.
+#![allow(unused, clippy::missing_errors_doc)]
 
-#![allow(unused)]
-
-use chrono::{DateTime, Local, NaiveDateTime, TimeZone, Utc};
+use chrono::{DateTime, Local, NaiveDateTime};
 
 pub const CUSTOM_FORMAT: &str = "%Y/%m/%d %H:%M";
 
-/// Get current local time as if it was UTC, in unix time.
 #[must_use]
 pub fn now_fake_utc() -> i64 {
     Local::now().naive_local().and_utc().timestamp()
 }
 
-/// Format the timestamp with the format string specified.
-///
-/// # Errors
-///
-/// This function will return an error if string formatting fails.
 pub fn format_str(timestamp: i64, format: &str) -> Result<String, String> {
     DateTime::from_timestamp(timestamp, 0)
         .map(|date| date.format(format).to_string())
         .ok_or(format!("unable to parse timestamp: {timestamp}"))
 }
 
-/// Parse a string with the format string specified, and return a timestamp.
-///
-/// # Errors
-///
-/// This function will return an error if string parsing fails.
 pub fn parse_str(time_str: &str, format: &str) -> Result<i64, String> {
     NaiveDateTime::parse_from_str(time_str, format)
         .map(|res| res.and_utc().timestamp())
