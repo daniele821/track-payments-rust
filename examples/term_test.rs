680bd057c7a6407c42ba3bd4902630ab00dd9114
Daniele Muffato <danixgithub1@gmail.com>
2025-03-31 10:49:17 +0200

    testing

--- examples/term_test.rs
+++ examples/term_test.rs
@@ -1,12 +1,34 @@
-use std::{thread::sleep, time::Duration};
+use std::{
+    thread::sleep,
+    time::{Duration, Instant},
+};
 
-use crossterm::terminal::{EnterAlternateScreen, disable_raw_mode, enable_raw_mode};
+use crossterm::{
+    event::{self, KeyCode, KeyModifiers},
+    terminal::{EnterAlternateScreen, disable_raw_mode, enable_raw_mode},
+};
 
 fn main() {
     enable_raw_mode().unwrap();
     let mut stdout = std::io::stdout();
+    let start = Instant::now();
     crossterm::execute!(stdout, EnterAlternateScreen).unwrap();
-    sleep(Duration::from_secs(10));
-    println!("ciao");
+    loop {
+        if event::poll(std::time::Duration::from_millis(100)).unwrap() {
+            if let event::Event::Key(key_event) = event::read().unwrap() {
+                println!("Key pressed: {:?}\r", key_event.code);
+                if key_event.code == KeyCode::Esc {
+                    break;
+                }
+                if key_event.code == KeyCode::Char('c')
+                    && key_event.modifiers.contains(KeyModifiers::CONTROL)
+                {
+                    println!("Ctrl+C detected. Exiting...");
+                    break;
+                }
+            }
+        }
+        println!("{}\r", start.elapsed().as_millis());
+    }
     disable_raw_mode().unwrap();
 }
