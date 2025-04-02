2600d3e4052ea93fe0b40e932dfe38ee68f9ad28
Daniele Muffato <danixgithub1@gmail.com>
2025-04-02 12:27:30 +0200

    improved bar chart renderer

--- src/tui_renderer/templates.rs
+++ src/tui_renderer/templates.rs
@@ -10,8 +10,10 @@ pub fn bar_graph_vertical(values: &[u32], width: u32, height: u32, cutout: u32) 
     let factor = actual_len / len;
     let unit_heigh = f64::from(height) / f64::from(max);
     let mut lines = Vec::with_capacity(height as usize);
-    println!("{len},{actual_len},{factor}");
+    let mut prev;
+    let cached_spaces = " ".repeat(factor - 1);
     for i in (1..=height).rev() {
+        prev = false;
         let mut str = String::with_capacity(actual_len);
         for j in values {
             if f64::from(i - 1) < f64::from(*j) * unit_heigh - 0.5 {
@@ -19,13 +21,22 @@ pub fn bar_graph_vertical(values: &[u32], width: u32, height: u32, cutout: u32) 
                 if *j >= cutout {
                     color = Color::DarkRed;
                 }
-                let tmp_str = format!("▏{}", " ".repeat(factor - 1))
+                let tmp_str = format!("▏{cached_spaces}")
                     .on(color)
                     .with(Color::Black)
                     .to_string();
                 str.push_str(&tmp_str);
+                prev = true;
             } else {
-                str.push_str(&" ".repeat(factor));
+                let mut sep = "▏";
+                if !prev {
+                    sep = " ";
+                }
+                let tmp_str = format!("{sep}{cached_spaces}")
+                    .with(Color::Black)
+                    .to_string();
+                str.push_str(&tmp_str);
+                prev = false;
             }
         }
         lines.push(str);
