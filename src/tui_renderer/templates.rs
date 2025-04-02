79e585530155b3678989c9c7179cd8107052bcbd
Daniele Muffato <danixgithub1@gmail.com>
2025-04-02 13:43:34 +0200

    very small optimization: move string creation inside if block

--- src/tui_renderer/templates.rs
+++ src/tui_renderer/templates.rs
@@ -2,14 +2,15 @@ use crossterm::style::{Color, Stylize};
 
 #[must_use]
 pub fn bar_graph_vertical(values: &[u32], width: u32, height: u32, cutout: u32) -> Vec<String> {
-    let empty_line = " ".repeat(width as usize);
     let mut lines = Vec::with_capacity(height as usize);
     if values.is_empty() {
+        let empty_line = " ".repeat(width as usize);
         for _ in 0..height {
             lines.push(empty_line.clone());
         }
         return lines;
     }
+
     let max = u32::max(1, *values.iter().max().unwrap_or(&0));
     let len = values.len();
     let actual_len = usize::max(len, width as usize / len * len);
@@ -17,6 +18,7 @@ pub fn bar_graph_vertical(values: &[u32], width: u32, height: u32, cutout: u32) 
     let unit_heigh = f64::from(height) / f64::from(max);
     let mut prev;
     let cached_spaces = " ".repeat(factor - 1);
+
     for i in (1..=height).rev() {
         prev = false;
         let mut str = String::with_capacity(actual_len);
