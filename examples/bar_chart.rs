6f7abfc69d5d8f8f62d4b719b74174d417d991fd
Daniele Muffato <danixgithub1@gmail.com>
2025-04-02 13:20:37 +0200

    func to check sizes are valid

--- examples/bar_chart.rs
+++ examples/bar_chart.rs
@@ -1,15 +1,18 @@
 fn main() {
     let width = crossterm::terminal::size().unwrap().0;
+    let width = u32::from(width);
+    let height = 30;
     let graph = track_payments_rust::tui_renderer::templates::bar_graph_vertical(
         &[
             0, 752, 707, 2787, 1019, 864, 890, 2853, 0, 0, 841, 989, 678, 990, 1812, 0, 733, 714,
             782, 931, 1722, 1803, 862, 1278, 1079, 857, 558, 1450, 536, 857, 649,
         ],
-        width.into(),
-        30,
+        width,
+        height,
         1000,
     );
     for line in graph {
         println!("{line}");
     }
+    assert!(track_payments_rust::tui_renderer::templates::are_sizes_valid(&graph, width, height));
 }
