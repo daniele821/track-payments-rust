a42fe3b91e6fc2c7ddb89e7eea7c6400ba82757f
Daniele Muffato <danixgithub1@gmail.com>
2025-04-05 19:26:24 +0200

    better crypto function + dep removal
    encrypt/decrypt functions now handle Vec<u8> instead of u8, as to allow
    binary files as input, instead of utf8 only. More space efficient, and
    allows removing `hex` dependency.
    
    Also removed `strsim` dependency, will add back once it is actually
    needed!

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,24 +1,23 @@
 use aes_gcm::aead::{Aead, AeadCore, KeyInit, OsRng};
 use aes_gcm::{Aes256Gcm, Key, Nonce};
 
-pub fn encrypt(key_str: &str, plaintext: &str) -> Option<String> {
-    let key = Key::<Aes256Gcm>::from_slice(key_str.as_bytes());
+pub fn encrypt(key_str: &[u8], plaintext: &[u8]) -> Option<Vec<u8>> {
+    let key = Key::<Aes256Gcm>::from_slice(key_str);
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
     let cipher = Aes256Gcm::new(key);
-    let ciphered_data = cipher.encrypt(&nonce, plaintext.as_bytes()).ok()?;
+    let ciphered_data = cipher.encrypt(&nonce, plaintext).ok()?;
     let mut encrypted_data: Vec<u8> = nonce.to_vec();
     encrypted_data.extend_from_slice(&ciphered_data);
-    Some(hex::encode(encrypted_data))
+    Some(encrypted_data)
 }
 
-pub fn decrypt(key_str: &str, encrypted_data: &str) -> Option<String> {
-    let encrypted_data = hex::decode(encrypted_data).ok()?;
-    let key = Key::<Aes256Gcm>::from_slice(key_str.as_bytes());
+pub fn decrypt(key_str: &[u8], encrypted_data: &[u8]) -> Option<Vec<u8>> {
+    let key = Key::<Aes256Gcm>::from_slice(key_str);
     let (nonce_arr, ciphered_data) = encrypted_data.split_at(12);
     let nonce = Nonce::from_slice(nonce_arr);
     let cipher = Aes256Gcm::new(key);
     let plaintext = cipher.decrypt(nonce, ciphered_data).ok()?;
-    String::from_utf8(plaintext).ok()
+    Some(plaintext)
 }
 
 #[cfg(test)]
@@ -27,8 +26,8 @@ mod tests {
 
     #[test]
     fn encryption() {
-        let plain_text = "Random text which i want to cipher!";
-        let key_str = "12345678901234567890123456789012";
+        let plain_text = "Random text which i want to cipher!".as_bytes();
+        let key_str = "12345678901234567890123456789012".as_bytes();
         let cipher_text = encrypt(key_str, plain_text).unwrap();
         let decipher_text = decrypt(key_str, &cipher_text).unwrap();
         assert_eq!(plain_text, decipher_text);
