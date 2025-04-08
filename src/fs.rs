e85f68afb7c6494fe5a72925b2b32cd84162fdf5
Daniele Muffato <danixgithub1@gmail.com>
2025-04-08 22:49:52 +0200

    panic if teorically unfallible operations fail (retrieving exe path/dir)

--- src/fs.rs
+++ src/fs.rs
@@ -3,21 +3,16 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use crate::error::{Error, Result};
-
-fn get_dir(path: &Path) -> Result<PathBuf> {
-    path.parent()
-        .map(|dir| dir.to_path_buf())
-        .ok_or_else(|| format!("unable to get parent of {}", path.to_string_lossy()))
-        .map_err(Error::from_generic)
+fn get_dir(path: &Path) -> Option<PathBuf> {
+    path.parent().map(|dir| dir.to_path_buf())
 }
 
-pub fn get_exe_path() -> Result<PathBuf> {
-    current_exe().map_err(Error::from_generic)
+pub fn get_exe_path() -> PathBuf {
+    current_exe().expect("failed to retrieve current executable")
 }
 
-pub fn get_exe_dir() -> Result<PathBuf> {
-    get_dir(&get_exe_path()?)
+pub fn get_exe_dir() -> PathBuf {
+    get_dir(&get_exe_path()).expect("failed to retrieve directory of current executable")
 }
 
 #[cfg(test)]
@@ -26,8 +21,8 @@ mod tests {
 
     #[test]
     fn get_exe() {
-        let exe_path = get_exe_path().unwrap();
-        let exe_dir = get_exe_dir().unwrap();
+        let exe_path = get_exe_path();
+        let exe_dir = get_exe_dir();
         println!("{exe_path:?}");
         println!("{exe_dir:?}");
     }
