241203b226f954f95f35b99219b3036559c832ef
Daniele Muffato <danixgithub1@gmail.com>
2025-04-08 22:16:20 +0200

    added thiserror + a func to easily convert all errors to my error

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,17 @@
 use std::error::Error as StdError;
+use std::result::Result as StdResult;
+use thiserror::Error;
 
+#[derive(Debug, Error)]
 pub enum Error {
+    #[error(transparent)]
     Generic(Box<dyn StdError>),
 }
 
-impl<T: StdError + 'static> From<T> for Error {
-    fn from(value: T) -> Self {
-        Self::Generic(Box::new(value))
+impl Error {
+    pub fn from_generic<T: StdError + 'static>(err: T) -> Self {
+        Self::Generic(Box::new(err))
     }
 }
+
+pub type Result<T> = StdResult<T, Error>;
