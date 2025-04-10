bdb13ef2a80e95e9e2686780803f303d69652c18
Daniele Muffato <danixgithub1@gmail.com>
2025-04-11 01:07:38 +0200

    crypto utils: str conversion

--- src/crypto.rs
+++ src/crypto.rs
@@ -14,6 +14,10 @@ pub fn encrypt(key_str: &[u8], plaintext: &[u8]) -> Result<Vec<u8>> {
     Ok(encrypted_data)
 }
 
+pub fn encrypt_str(key_str: &[u8], plaintext: &str) -> Result<Vec<u8>> {
+    encrypt(key_str, plaintext.as_bytes())
+}
+
 pub fn decrypt(key_str: &[u8], encrypted_data: &[u8]) -> Result<Vec<u8>> {
     let key = Key::<Aes256Gcm>::from_slice(key_str);
     let (nonce_arr, ciphered_data) = encrypted_data.split_at(12);
@@ -24,9 +28,13 @@ pub fn decrypt(key_str: &[u8], encrypted_data: &[u8]) -> Result<Vec<u8>> {
         .map_err(|_| Error::DecryptionFailed)
 }
 
+pub fn decrypt_str(key_str: &[u8], encrypted_data: &[u8]) -> Result<String> {
+    String::from_utf8(decrypt(key_str, encrypted_data)?).map_err(Error::from_generic)
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{decrypt, encrypt};
+    use super::{decrypt, decrypt_str, encrypt, encrypt_str};
 
     #[test]
     fn encryption() {
@@ -34,6 +42,15 @@ mod tests {
         let key_str = "12345678901234567890123456789012".as_bytes();
         let cipher_text = encrypt(key_str, plain_text).unwrap();
         let decipher_text = decrypt(key_str, &cipher_text).unwrap();
+        assert_eq!(plain_text, decipher_text);
+    }
+
+    #[test]
+    fn encryption_str() {
+        let plain_text = "Random text which i want to cipher!";
+        let key_str = "12345678901234567890123456789012".as_bytes();
+        let cipher_text = encrypt_str(key_str, plain_text).unwrap();
+        let decipher_text = decrypt_str(key_str, &cipher_text).unwrap();
         assert_eq!(plain_text, decipher_text);
     }
 }
