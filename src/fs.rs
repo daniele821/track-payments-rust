4b9ce358c4ca88347688b88de0f766d314a62943
Daniele Muffato <danixgithub1@gmail.com>
2025-04-12 10:36:25 +0200

    not use namespace

--- src/fs.rs
+++ src/fs.rs
@@ -2,6 +2,7 @@ use crate::error::{Error, Result};
 use std::{
     env::current_exe,
     fs::read,
+    fs::write,
     path::{Path, PathBuf},
 };
 
@@ -24,7 +25,7 @@ pub fn read_file<P: AsRef<Path>>(path: P) -> Result<Vec<u8>> {
 }
 
 pub fn write_file<P: AsRef<Path>>(path: P, contents: Vec<u8>) -> Result<()> {
-    std::fs::write(path, contents).map_err(Error::FileError)
+    write(path, contents).map_err(Error::FileError)
 }
 
 #[cfg(test)]
